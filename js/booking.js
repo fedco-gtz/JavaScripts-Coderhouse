f21804bcaa8b94bf0fbdd0df34e063121c5bde5b
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2024-03-11 20:21:29 -0300

    ProyectoFinal-Gutierrez

--- js/booking.js
+++ js/booking.js
@@ -1,41 +1,36 @@
+// |-------------------------------|
+// | Código que muestra mis vuelos |
+// |-------------------------------|
+document.addEventListener("DOMContentLoaded", () => {
+    const CODIGORESERVA = localStorage.getItem('codigoReserva');
+    const DATOSSELECCIONADOS = localStorage.getItem('datosSeleccionados');
+    const USERDATA = localStorage.getItem('userData');
 
-/*
-/ Obtener los datos del localStorage
-let datosSeleccionados = JSON.parse(localStorage.getItem('datosSeleccionados')) || [];
-let userData = JSON.parse(localStorage.getItem('userData')) || [];
-let codigoReserva = localStorage.getItem('codigoReserva') || '';
+    if (CODIGORESERVA && DATOSSELECCIONADOS && USERDATA) {
+        const DATOSSELECCIONADOSOBJ = JSON.parse(DATOSSELECCIONADOS);
+        const USERDATAOBJ = JSON.parse(USERDATA);
 
-// Almacenar el código de reserva en un array
-let codigosReserva = [];
-if (codigoReserva !== '') {
-    codigosReserva.push(codigoReserva);
-}
-
-// Limpiar el localStorage
-localStorage.removeItem('datosSeleccionados');
-localStorage.removeItem('userData');
-localStorage.removeItem('codigoReserva');
-
-// Ahora los datos están en tres arrays distintas y el localStorage está limpio
-console.log(datosSeleccionados);
-console.log(userData);
-console.log(codigosReserva);
-
-*/
-// Obtener los datos del localStorage
-let datosSeleccionados = JSON.parse(localStorage.getItem('datosSeleccionados')) || [];
-let userData = JSON.parse(localStorage.getItem('userData')) || [];
-let codigoReserva = localStorage.getItem('codigoReserva') || '';
-
-// Almacenar el código de reserva en un array
-let codigosReserva = codigoReserva !== '' ? [codigoReserva] : [];
-
-// Limpiar el localStorage
-localStorage.removeItem('datosSeleccionados');
-localStorage.removeItem('userData');
-localStorage.removeItem('codigoReserva');
-
-// Ahora los datos están en tres arrays distintas y el localStorage está limpio
-console.log(datosSeleccionados);
-console.log(userData);
-console.log(codigosReserva);
+        const divContent = document.createElement('div');
+        divContent.innerHTML = `
+        <h4><b>RESERVA ${CODIGORESERVA}</b></h4>
+        <h5><b>VUELO</b></h5>
+            <ul>
+                <li><b>ORIGEN:</b> ${DATOSSELECCIONADOSOBJ.origen} - <b>DESTINO:</b> ${DATOSSELECCIONADOSOBJ.destino}</li>
+                <li><b>PASAJEROS:</b> ${DATOSSELECCIONADOSOBJ.cantidadPersonas} - <b>CLASE:</b> ${DATOSSELECCIONADOSOBJ.clase} - <b>TIPO VUELO:</b> ${DATOSSELECCIONADOSOBJ.tipoVuelo.toUpperCase()}</li>
+                <li><b>FECHA SALIDA:</b> ${DATOSSELECCIONADOSOBJ.fecha} - <b>FECHA REGRESO:</b> ${DATOSSELECCIONADOSOBJ.fechaRegreso}</li>
+                <li><b>HORA SALIDA:</b> ${DATOSSELECCIONADOSOBJ.horaVuelo} - <b>HORA REGRESO:</b> INFORMACIÓN NO DISPONIBLE</li>
+            </ul>
+            <h5><b>PAGO</b></h5>
+            <ul>
+            <li>TARJETA <b>${USERDATAOBJ.emisor}</b> TERMINADA EN <b>${USERDATAOBJ.numeroTarjeta.slice(-4)}</b></li>
+            <li><b>TIPO DE PAGO:</b> ${USERDATAOBJ.tipoPago.slice(7).toUpperCase()} - <b>CUOTA:</b> ${USERDATAOBJ.cuotasCredito}</li>
+        </ul>`;
+        document.getElementById('divVisible').appendChild(divContent);
+        document.getElementById('divVisible').style.display = 'block';
+    } else {
+        const divContent = document.createElement('div');
+        divContent.innerHTML = `<p> <b>No hay información de vuelos disponible</b> </p>`
+        document.getElementById('divVisible').appendChild(divContent);
+        document.getElementById('divVisible').style.display = 'block';
+    }
+});
