4f0bfc05e392a73500eecb98e89d2a77b65a2c58
Federico Gabriel Gutierrez <gutierrezfedericog@gmail.com>
2024-03-11 23:26:39 -0300

    ProyectoFinal-Gutierrez

--- js/login.js
+++ js/login.js
@@ -2,7 +2,7 @@
 // |     Código que simula el inicio de sesión      |
 // |------------------------------------------------|
 document.addEventListener("DOMContentLoaded", function() {
-  var username = localStorage.getItem('username');
+  let username = localStorage.getItem('username');
   if (username) {
     showWelcomeMessage(username);
   } else {
@@ -15,7 +15,7 @@ function closeLoginPopup() {
 }
 
 function showWelcomeMessage(username) {
-  var welcomeDiv = document.getElementById("welcomeMessage");
+  let welcomeDiv = document.getElementById("welcomeMessage");
   welcomeDiv.innerHTML = `<b>${username.toUpperCase()}</b>, HOLA DE NUEVO  |  <button onclick="cerrarSesion()">CERRAR SESIÓN</button>`;
   welcomeDiv.style.display = "block";
   localStorage.setItem('username', username);
@@ -23,19 +23,19 @@ function showWelcomeMessage(username) {
 }
 
 function showUserNotFoundMessage() {
-  var userNotFoundDiv = document.getElementById("userNotFoundMessage");
+  let userNotFoundDiv = document.getElementById("userNotFoundMessage");
   userNotFoundDiv.style.display = "block";
 }
 
 function showUserNotFoundGoogle() {
-  var userNotFoundGoogleDiv = document.getElementById("userNotFoundGoogle");
+  let userNotFoundGoogleDiv = document.getElementById("userNotFoundGoogle");
   if (userNotFoundGoogleDiv) {
     userNotFoundGoogleDiv.style.display = "block";
   }
 }
 
 function login() {
-  var username = document.getElementById("username").value;
+  let username = document.getElementById("username").value;
 
   fetch('https://09cf195c04ea43a6b52f65caee85d0c7.api.mockbin.io/')
     .then(response => response.json())
